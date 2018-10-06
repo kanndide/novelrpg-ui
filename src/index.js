1dab4f16d6cf835580720f8b2b45eafb3bd0c1f7
kanndide <kanndide@gmail.com>
2018-10-06 10:04:43 +0300

    removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk';
 import { store } from './_helpers/store';
 import * as serviceWorker from './serviceWorker';
 
