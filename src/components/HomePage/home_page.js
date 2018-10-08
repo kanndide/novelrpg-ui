f2fd0399ac99103d91d616afe0de3034c2383912
kanndide <kanndide@gmail.com>
2018-10-08 17:23:17 +0300

    homepage added for user

---
+++ src/components/HomePage/home_page.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userActions } from '../../_actions/user_actions';
+
+class HomePage extends React.Component {
+
+    handleDeleteUser(id) {
+        return (e) => this.props.dispatch(userActions.delete(id));
+    }
+
+    render() {
+        const { user } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hi {user.email}!</h1>
+                <p>You're logged in with React!!</p>
+                <button onClick={this.handleDeleteUser(user.id)}>Delete</button>
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { authentication } = state;
+    const { user } = authentication;
+    return {
+        user
+    };
+}
+
+const connectedHomePage = connect(mapStateToProps)(HomePage);
+export { connectedHomePage as HomePage };
