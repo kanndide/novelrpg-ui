d1053150190fa53c95cd25d76d727324642df95b
kanndide <kanndide@gmail.com>
2018-10-08 14:25:16 +0300

    modified for routes and navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Navbar from '../components/NavBar/navbar'
+import { history } from '../_helpers/history';
+import { alertActions } from '../_actions/alert_actions';
+import { PrivateRoute } from '../components/private_route';
+import { RegisterPage } from '../components/RegisterPage/register_page';
+import { LoginPage } from '../components/LoginPage/login_page'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>You need to put something here, dawg.</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message && <div className={`alert ${alert.type}`}>{alert.message}</div>}
+                        <Router history={history}>
+                            <div>
+                            	<Navbar />
+                            	<h1>NovelRPG</h1>
+                                <Route path="/signup" component={RegisterPage} />
+                                <Route path="/login" component={LoginPage} />
+                            </div>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+export default connect(mapStateToProps)(App);; 
