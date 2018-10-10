293712b36166c92285eb568f66e55648894af7d1
kanndide <kanndide@gmail.com>
2018-10-10 08:21:16 +0300

    styles and structure added

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -8,41 +8,72 @@ const link = {
   background: 'blue',
   textDecoration: 'none',
   color: 'white',
+  textAlign: 'center',
+  fontSize: 'medium',
 }
-const Navbar = (props) =>
-  <div>
-    <NavLink
-      to="/"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Home</NavLink>
-    <NavLink
-      to="/signup"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Sign Up</NavLink>
-    <NavLink
-      to="/login"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Login</NavLink>
-    <NavLink
-      to="/login"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Logout</NavLink>
+
+const div = {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  zIndex: '999',
+  width: '100%',
+  border: '1px solid black',
+  padding: '5px',
+  background: 'SlateGrey',
+}
+
+const linkDiv = {
+  margin: 'auto',
+  alignItems: 'center',
+}
+
+const Navbar = () =>
+  <div id="navbar" className="col-sm-8" style={div}>
+    <div className="col-sm-3" style={linkDiv}>
+      <NavLink
+        to="/"
+        exact
+        style={link}
+        className="col-sm-3"
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Home</NavLink>
+    </div>
+    <div className="col-sm-3">
+      <NavLink
+        to="/signup"
+        exact
+        style={link}
+        className="col-sm-3"
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Sign Up</NavLink>
+    </div>
+    <div className="col-sm-3">
+      <NavLink
+        to="/login"
+        exact
+        style={link}
+        className="col-sm-3"
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Login</NavLink>
+    </div>
+    <div className="col-sm-3">
+      <NavLink
+        to="/login"
+        exact
+        style={link}
+        className="col-sm-3"
+        activeStyle={{
+          background: 'darkblue'
+        }}
+      >Logout</NavLink>
+    </div>
   </div>;
 
 export default Navbar
