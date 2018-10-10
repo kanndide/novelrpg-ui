59e8e944545b4b145dcf0bc67a19a596b155b61a
kanndide <kanndide@gmail.com>
2018-10-10 08:18:58 +0300

    changed to support user deletion

--- src/reducers/users_reducer.js
+++ src/reducers/users_reducer.js
@@ -15,35 +15,15 @@ export function users(state = {}, action) {
         error: action.error
       };
     case userConstants.DELETE_REQUEST:
-      // add 'deleting:true' property to user being deleted
-      return {
-        ...state,
-        items: state.items.map(user =>
-          user.id === action.id
-            ? { ...user, deleting: true }
-            : user
-        )
-      };
+      let user = JSON.parse(localStorage.getItem('user'))
+      return { ...state, user };
     case userConstants.DELETE_SUCCESS:
-      // remove deleted user from state
+      debugger
       return {
         items: state.items.filter(user => user.id !== action.id)
       };
     case userConstants.DELETE_FAILURE:
-      // remove 'deleting:true' property and add 'deleteError:[error]' property to user 
-      return {
-        ...state,
-        items: state.items.map(user => {
-          if (user.id === action.id) {
-            // make copy of user without 'deleting:true' property
-            const { deleting, ...userCopy } = user;
-            // return copy of user with 'deleteError:[error]' property
-            return { ...userCopy, deleteError: action.error };
-          }
-
-          return user;
-        })
-      };
+      return { error: action.error};
 
     case userConstants.GET_USER_REQUEST:
       return {
